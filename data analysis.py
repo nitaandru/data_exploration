efab1abb0d2247d6a87ec472884aa1ff80aba3e4
Andrei <39564825+nitaandru@users.noreply.github.com>
2022-06-29 16:25:22 +0300

    Update data analysis.py

--- data analysis.py
+++ data analysis.py
@@ -2,6 +2,7 @@ import pandas as pd
 import glob
 import numpy as np
 import seaborn as sns
+from scipy import stats
 
 # get data file names
 path ='C:/Nita/UGAL/ECA_non-blended_custom/'
@@ -39,12 +40,13 @@ frame.TG.describe()
 frame = frame.loc[frame["DATE"].between("1961-01-01", "2020-12-31")]
 
 # read the metadata for stations
-meta = pd.read_csv("C:/Nita/UGAL/ECA_non-blended_custom/sources.txt", skiprows = 23)
+meta = pd.read_table("C:/Nita/UGAL/ECA_non-blended_custom/sources.txt", skiprows = 23, delimiter = '\t|,')
 meta.head(5)
 
 # select only the name and coordinates then name the columns
 meta = meta.iloc[:,[1,2,4,5,6]]
 meta.columns = ['SOUID', 'SOUNAME', 'LAT', 'LON', 'HEIGHT']
+meta.SOUNAME = meta.SOUNAME.replace(r'\s+', ' ', regex=True) # remove extra whitespaces in the stations name
 
 # join temperature data with stations names
 new_data = pd.merge(frame, meta.iloc[:,[0,1]], how = 'inner') 
@@ -68,21 +70,37 @@ ax = sns.barplot(x="PERCENT", y="SOUNAME", data = misses)
 misses_year['YEAR'] =  misses_year['YEAR'].astype(int)
 
 g = sns.FacetGrid(misses_year, col='SOUNAME', hue='SOUNAME', col_wrap=8)
-
 # Add the line over the area with the plot function
 g = g.map(plt.plot, 'YEAR', 'Count')
- 
 # Fill the area with fill_between
 g = g.map(plt.fill_between, 'YEAR', 'Count', alpha=0.2).set_titles("{col_name} station")
- 
 # Control the title of each facet
 g = g.set_titles("{col_name}")
- 
 # Add a title for the whole plot
 plt.subplots_adjust(top=0.92)
-# g = g.fig.suptitle('Evolution of the value of stuff in 16 countries')
-
 # Show the graph
 plt.show()
 
 # toate statiile au lipsa informatii pentru 2020.
+# unele statii au informatii lipsa random (Bacau), altele non-aleatoriu (Baia Mare, Turnu Magurele sau Ocna-Sugatag)
+
+## Exemplul 1: inlocuim valorile lipsa cu media sirului (ex: Ocna Sugatag).
+ex1 = new_data[new_data["SOUNAME"] == "OCNA SUGATAG "]
+
+# aggregate to annual mean
+ex1_an = ex1['TG'].resample('A').mean()
+round(ex1_an.mean(),2) # get the multianual mean
+
+# calculeaza panta cu lipsuri
+x = ex1_an.dropna() # drop the missing to calculate the Sen slope
+y = range(1960, 2019)
+
+res = stats.theilslopes(x, y, 0.95)
+round(res[0] * 60,2) # panta medie a schimbarii pe deceniu
+
+# calculeaza panta fara lipsuri
+x = ex1_an.fillna(ex1_an.mean()) # replace the missing with multiannual mean
+y = range(1960, 2020)
+
+res = stats.theilslopes(x, y, 0.95)
+round(res[0] * 60,2) 
