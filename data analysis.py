f13cef3e24854731bdeb0d9b398d9a7243fee777
Andrei <39564825+nitaandru@users.noreply.github.com>
2022-07-05 17:03:39 +0300

    Update data analysis.py

--- data analysis.py
+++ data analysis.py
@@ -186,3 +186,83 @@ with pd.ExcelWriter('/home/nitaandru/Desktop/ugal/export/climate_data_RO.xlsx') 
     lunare.to_excel(writer, sheet_name='lunare')
     misses.to_excel(writer, sheet_name = 'missing')
     
+## Pentru KDE plots   
+# getting necessary libraries
+sns.set_theme(style="white", rc={"axes.facecolor": (0, 0, 0, 0)})
+
+temp = pd.read_csv('/home/nitaandru/Desktop/ugal/TG_SOUID107510.txt', skiprows = 19, header = 0,  na_values = -9999)
+temp.columns = ['STAID', 'SOUID', 'DATE', 'TG', 'Q_TG'] 
+temp['DATE'] = pd.to_datetime(temp['DATE'], format = "%Y%m%d")
+temp = temp.set_index('DATE', drop = False)
+temp.TG = temp.TG/ 10
+
+temp['month'] = pd.to_datetime(temp['DATE']).dt.month
+temp['b'] =  pd.to_datetime(temp.index).strftime('%m')
+
+# we define a dictionnary with months that we'll use later
+month_dict = {1: 'january',
+              2: 'february',
+              3: 'march',
+              4: 'april',
+              5: 'may',
+              6: 'june',
+              7: 'july',
+              8: 'august',
+              9: 'september',
+              10: 'october',
+              11: 'november',
+              12: 'december'}
+
+# we create a 'month' column
+temp['month'] = temp['month'].map(month_dict)
+
+# we generate a pd.Serie with the mean temperature for each month (used later for colors in the FacetGrid plot), and we create a new column in temp dataframe
+month_mean_serie = temp.groupby('month')['TG'].mean()
+temp['mean_month'] = temp['month'].map(month_mean_serie)
+
+# we generate a color palette with Seaborn.color_palette()
+pal = sns.color_palette(palette='coolwarm', n_colors=12)
+
+# in the sns.FacetGrid class, the 'hue' argument is the one that is the one that will be represented by colors with 'palette'
+g = sns.FacetGrid(temp, row='month', hue='mean_month', aspect=15, height=0.75, palette=pal)
+
+# then we add the densities kdeplots for each month
+g.map(sns.kdeplot, 'TG',
+      bw_adjust=1, clip_on=False,
+      fill=True, alpha=1, linewidth=1.5)
+
+# here we add a white line that represents the contour of each kdeplot
+g.map(sns.kdeplot, 'TG', 
+      bw_adjust=1, clip_on=False, 
+      color="w", lw=2)
+
+# here we add a horizontal line for each plot
+g.map(plt.axhline, y=0,
+      lw=2, clip_on=False)
+
+# we loop over the FacetGrid figure axes (g.axes.flat) and add the month as text with the right color
+# notice how ax.lines[-1].get_color() enables you to access the last line's color in each matplotlib.Axes
+for i, ax in enumerate(g.axes.flat):
+    ax.text(-15, 0.02, month_dict[i+1],
+            fontweight='bold', fontsize=15,
+            color=ax.lines[-1].get_color())
+    
+# we use matplotlib.Figure.subplots_adjust() function to get the subplots to overlap
+g.fig.subplots_adjust(hspace=-0.3)
+
+# eventually we remove axes titles, yticks and spines
+g.set_titles("")
+g.set(yticks=[])
+g.despine(bottom=True, left=True)
+
+g.set(ylabel='')
+
+plt.setp(ax.get_xticklabels(), fontsize=15, fontweight='bold')
+plt.xlabel('Temperatura in grade Celsius', fontweight='bold', fontsize=15)
+g.fig.suptitle('Media zilnica lunara a temperaturii in Galati',
+               ha='right',
+               fontsize=20,
+               fontweight=20)
+
+plt.show()
+
